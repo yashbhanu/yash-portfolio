b82300c23cc56c30d0dac4e514281db73281da92
yashbhanu <bhanushaliyash2000@gmail.com>
2024-05-10 00:04:01 +0530

    aboout me section started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,14 +33,71 @@ export default function Home() {
             </p>
 
             <p className="text-2xl lg:text-4xl lg:leading-[3.2rem] leading-[2.5rem] 2xl:w-11/12 w-full">
-              I design, code and engineer software applications while writing about code
+              I design, code and engineer software applications while writing
+              about code
             </p>
 
             <button className="border border-darkBlue md:px-10 md:py-3 px-6 py-2 rounded-full text-xl text-darkBlue mt-6">
               Resume
             </button>
           </div>
-          <Image className="xl:w-80 lg:w-72 md:w-60" src={Intro} alt="github" />
+          <Image className="lg:w-80 w-72" src={Intro} alt="github" />
+        </div>
+      </section>
+
+      <section className="sm:p-8 p-2 m-auto text-center">
+        <span className="text-3xl text-medGrey ">about me</span>
+
+        <div className="flex flex-col md:flex-row gap-16 2xl:px-40 xl:px-32 lg:px-28 md:px-14 sm:px-8 px-6 mt-20">
+          <div className="w-full lg:w-1/2">
+            <p className="text-xl text-medGrey text-left">
+              I am a Full Stack Developer passionate in creating seamless web
+              applications which exceeds expectations with a knack for crafting
+              optimized solutions.
+            </p>
+            <p className="text-xl mt-8 text-medGrey text-left">
+              I design, code, and engineer digital experiences with a focus on
+              user-centric design create solutions that are not just functional,
+              but also beautiful and intuitive.
+            </p>
+          </div>
+
+          <div className="w-full lg:w-1/2 flex flex-col gap-8">
+            <p className="text-2xl text-medGrey text-left">skills</p>
+            <div className="flex flex-wrap gap-3">
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">Javascript</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">ReactJs</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">NextJs</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">NodeJs</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">MongoDB</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">Docker</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 flex items-center">
+                <p className="text-darkBlue">NestJs</p>
+              </div>
+
+              <div className="border-2 border-darkBlue rounded-full text-darkBlue px-6 py-1">
+                <p className="text-darkBlue">Git</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
